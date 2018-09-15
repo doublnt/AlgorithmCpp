eaaf6e92ffbdd935e9913b3b3e47bfbf8fe58b91
doublnt <yinximail@gmail.com>
2018-09-15 08:33:47 +0800

    Add unsigned int test

---
+++ DS_Algo_Ana/chapter03/unsignedinttest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+	unsigned int a = 100;
+	int b = -100;
+
+	cout << "unsigned int:" << a <<endl;
+	cout << "int:" << b << endl;
+
+	printf("unsigned int: %u\n",a);
+	printf("int %d",b);
+}
